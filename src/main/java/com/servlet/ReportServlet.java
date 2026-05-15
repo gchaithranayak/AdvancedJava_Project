cece9b32ca54d3f9ae452c30a48ddf6b8dcbad69
gchaithranayak <gchaithranayak@gmail.com>
2026-05-15 10:05:21 +0530

    Update ReportServlet.java

--- src/main/java/com/servlet/ReportServlet.java
+++ src/main/java/com/servlet/ReportServlet.java
@@ -1,33 +1,57 @@
 package com.servlet;
 
-import java.io.*;
+import java.io.IOException;
+import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.util.*;
+
 import com.dao.HospitalDAO;
 import com.model.Patient;
 
 public class ReportServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
+            throws ServletException, IOException {  
 
         String type = req.getParameter("type");
+
         HospitalDAO dao = new HospitalDAO();
         List<Patient> list = new ArrayList<>();
 
-        if("doctor".equals(type)) {
-            list = dao.getByDoctor(req.getParameter("value"));
-        } else if("ailment".equals(type)) {
-            list = dao.getByAilment(req.getParameter("value"));
-        } else {
-            list = dao.getByDateRange(
-                req.getParameter("from"),
-                req.getParameter("to")
-            );
+        // 🔍 Debug (optional - remove later)
+        System.out.println("TYPE = " + type);
+
+        if ("doctor".equals(type)) {
+
+            String value = req.getParameter("value");
+            System.out.println("Doctor = " + value);
+
+            list = dao.getByDoctor(value);
+
+        } else if ("ailment".equals(type)) {
+
+            String value = req.getParameter("value");
+            System.out.println("Ailment = " + value);
+
+            list = dao.getByAilment(value);
+
+        } else if ("date".equals(type)) {
+
+            String from = req.getParameter("from");
+            String to = req.getParameter("to");
+
+            System.out.println("FROM = " + from);
+            System.out.println("TO = " + to);
+
+            // 🚨 Safety check
+            if (from != null && to != null && !from.isEmpty() && !to.isEmpty()) {
+                list = dao.getByDateRange(from, to);
+            }
         }
 
+        // Send result to JSP
         req.setAttribute("data", list);
+
         RequestDispatcher rd = req.getRequestDispatcher("report_result.jsp");
         rd.forward(req, res);
     }
