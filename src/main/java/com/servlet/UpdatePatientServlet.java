1d7f52bda54e7fd0a4acfaede5f8fc9941676dc9
gchaithranayak <gchaithranayak@gmail.com>
2026-05-15 10:05:53 +0530

    Update UpdatePatientServlet.java

--- src/main/java/com/servlet/UpdatePatientServlet.java
+++ src/main/java/com/servlet/UpdatePatientServlet.java
@@ -1,34 +1,75 @@
 package com.servlet;
 
 
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 import com.dao.HospitalDAO;
 import com.model.Patient;
 
 public class UpdatePatientServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+
+    protected void doPost(HttpServletRequest req,
+                          HttpServletResponse res)
             throws ServletException, IOException {
 
         try {
+
+            // Doctor dropdown handling
+            String doctor = req.getParameter("doctor");
+
+            if(doctor.equals("Other")) {
+
+                doctor = req.getParameter("otherDoctor");
+            }
+
+            // Disease dropdown handling
+            String ailment = req.getParameter("ailment");
+
+            if(ailment.equals("Other")) {
+
+                ailment = req.getParameter("otherDisease");
+            }
+
             Patient p = new Patient(
+
                 Integer.parseInt(req.getParameter("id")),
+
                 req.getParameter("name"),
-                Integer.parseInt(req.getParameter("age")),
-                "",
-                "",
-                req.getParameter("ailment"),
-                req.getParameter("doctor")
+
+                req.getParameter("dob"),
+
+                req.getParameter("gender"),
+
+                Double.parseDouble(req.getParameter("weight")),
+
+                req.getParameter("date"),
+
+                ailment,
+
+                doctor
             );
 
             HospitalDAO dao = new HospitalDAO();
-            dao.updatePatient(p);
 
-            res.sendRedirect("patientdisplay.jsp");
+            if(dao.updatePatient(p)) {
+
+                res.sendRedirect("patientdisplay.jsp");
+
+            } else {
+
+                res.getWriter().println("Update Failed!");
+
+            }
 
         } catch (Exception e) {
+
             e.printStackTrace();
+
+            res.getWriter().println("Error Occurred!");
+
         }
     }
 }
